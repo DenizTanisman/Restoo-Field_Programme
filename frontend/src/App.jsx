672277e68c678dca0e71abe59f9e8c2680d8a36a
caner <caneroyku@outlook.com>
2026-03-23 11:22:53 +0300

    folder structre edited, Theme Toggle component added daisy UI Tailwindcss installations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ThemeToggle from "./components/ThemeToggle";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      App
+      <ThemeToggle></ThemeToggle>
+    </div>
+  );
+};
+
+export default App;
