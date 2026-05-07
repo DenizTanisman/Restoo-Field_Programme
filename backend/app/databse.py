6ae834e0b2514ba226533f62883313af73c8c986
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2026-05-08 00:08:37 +0300

    Backend temel yapısı kuruldu, async veritabanı ve Alembic altyapısı eklendi

---
+++ backend/app/databse.py
@@ -0,0 +1,22 @@
+from app.config import settings
+from sqlalchemy.ext.asyncio import(create_async_engine,async_sessionmaker,AsyncSession)
+from sqlalchemy import declarative_base
+
+
+DATABASE_URL= settings.DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL,echo=True)
+
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_= AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+      yield session
